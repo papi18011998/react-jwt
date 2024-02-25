6bda9cf1b913d244ee64737b1b1ebeaa4dce94e8
pindiaye <papaibrahima98@gmail.com>
2024-02-25 01:23:12 +0000

    design login page && simulate non auth user

--- src/_helpers/AuthGuard.js
+++ src/_helpers/AuthGuard.js
@@ -10,3 +10,31 @@ const AuthGuard = ({children}) => {
 };
 
 export default AuthGuard;
+
+// import React from 'react';
+// import { Route, Redirect } from 'react-router-dom';
+// import jwt from 'jsonwebtoken';
+
+// const PrivateRoute = ({ component: Component, ...rest }) => (
+//   <Route
+//     {...rest}
+//     render={(props) => {
+//       const token = localStorage.getItem('token');
+//       if (!token) {
+//         // No JWT found, redirect to login page
+//         return <Redirect to="/login" />;
+//       }
+//       try {
+//         // Verify the JWT
+//         jwt.verify(token, secretKey);
+//         // JWT is valid, render the protected component
+//         return <Component {...props} />;
+//       } catch (error) {
+//         // JWT is invalid, redirect to login page
+//         return <Redirect to="/login" />;
+//       }
+//     }}
+//   />
+// );
+
+// export default PrivateRoute;
--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,30 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+    const [credentials, setCredentials] = useState({
+        login:'',
+        password:''
+    })
+
+    const onChange = (e)=>{
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const onSubmit = (e)=>{
+        e.preventDefault();
+        console.log('stop submitting');
+    }
+
     return (
-        <section class="vh-100">
-            <div class="container py-5 h-100">
-                <div class="row d-flex align-items-center justify-content-center h-100">
-                    <div class="col-md-8 col-lg-7 col-xl-6">
+        <section className="vh-100">
+            <div className="container py-5 h-100">
+                <div className="row d-flex align-items-center justify-content-center h-100">
+                    <div className="col-md-8 col-lg-7 col-xl-6">
                         <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-                        class="img-fluid" alt="Phone image"/>
+                        className="img-fluid" alt="Phone image"/>
                     </div>
-                    <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
-                        <form>
+                    <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+                        <form onSubmit={onSubmit}>
                         {/* Email input */}
-                        <div class="form-outline mb-4">
-                            <label class="form-label" for="form1Example13">Email address</label>
-                            <input type="email" id="form1Example13" class="form-control form-control-lg" />
+                        <div className="form-outline mb-4">
+                            <label className="form-label" htmlFor="form1Example13">Email address</label>
+                            <input 
+                                type="text" 
+                                id="form1Example13" 
+                                className="form-control form-control-lg"
+                                name='login'
+                                value={credentials.login}
+                                onChange={onChange}
+                            />
                         </div>
 
                         {/* Password input  */}
-                        <div class="form-outline mb-4">
-                            <label class="form-label" for="form1Example23">Password</label>
-                            <input type="password" id="form1Example23" class="form-control form-control-lg" />
+                        <div className="form-outline mb-4">
+                            <label className="form-label" htmlFor="form1Example23">Password</label>
+                            <input 
+                                type="password" 
+                                id="form1Example23" 
+                                className="form-control form-control-lg"
+                                name='password'
+                                value={credentials.password}
+                                onChange={onChange}
+                            />
                         </div>
 
                         {/* Submit button */}
-                        <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
+                        <button type="submit" className="btn btn-primary btn-lg btn-block">Connexion</button>
 
                         </form>
                     </div>
